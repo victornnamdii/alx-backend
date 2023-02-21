929e4d637f37c193e67a22d45fa91dc66795ebba
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-21 20:48:45 +0100

    Task 3 w/ less memory space

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -10,28 +10,18 @@ class LRUCache(BaseCaching):
     """
     LRU Caching System
     """
-    used = 0
-
-    def __init__(self):
-        super().__init__()
-        self.lru_data = {}
-
     def put(self, key, item):
         """
         Add an item to the cache
         """
         if key is not None and item is not None:
+            if key in self.cache_data:
+                del self.cache_data[key]
             self.cache_data[key] = item
-            self.lru_data[key] = self.used
-            self.used += 1
             if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                lru = min(self.lru_data.values())
-                for k, v in self.lru_data.items():
-                    if v == lru:
-                        break
-                del self.cache_data[k]
-                del self.lru_data[k]
-                print("DISCARD: {}".format(k))
+                first_key = next(iter(self.cache_data))
+                del self.cache_data[first_key]
+                print("DISCARD: {}".format(first_key))
 
     def get(self, key):
         """
@@ -39,6 +29,7 @@ class LRUCache(BaseCaching):
         """
         if key is None or key not in self.cache_data:
             return None
-        self.lru_data[key] = self.used
-        self.used += 1
+        item = self.cache_data[key]
+        del self.cache_data[key]
+        self.cache_data[key] = item
         return self.cache_data[key]
