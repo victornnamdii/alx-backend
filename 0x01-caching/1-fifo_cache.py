a8323e8e00d278df713a545fef60c109ba0960c4
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-21 19:25:25 +0100

    Task 0-2

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python3
 """
-Learning caching
+Learning Caching
 """
 
 from base_caching import BaseCaching
 
 
-class BasicCache(BaseCaching):
+class FIFOCache(BaseCaching):
     """
-    Basic caching
+    Fifo Caching System
     """
     def __init__(self):
-        """
-        Initialization
-        """
         super().__init__()
 
     def put(self, key, item):
         """
-        Add an item in the cache
+        Add an item to the cache
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                first_key = next(iter(self.cache_data))
+                self.cache_data.pop(first_key)
+                print("DISCARD: {}".format(first_key))
 
     def get(self, key):
         """
--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,26 +1,26 @@
 #!/usr/bin/env python3
 """
-Learning caching
+Learning Caching
 """
 
 from base_caching import BaseCaching
 
 
-class BasicCache(BaseCaching):
+class LIFOCache(BaseCaching):
     """
-    Basic caching
+    LIFO Caching System
     """
-    def __init__(self):
-        """
-        Initialization
-        """
-        super().__init__()
-
     def put(self, key, item):
         """
-        Add an item in the cache
+        Add an item to the cache
         """
         if key is not None and item is not None:
+            if key not in self.cache_data:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    discarded = self.cache_data.popitem()[0]
+                    print("DISCARD: {}".format(discarded))
+            else:
+                del self.cache_data[key]
             self.cache_data[key] = item
 
     def get(self, key):
