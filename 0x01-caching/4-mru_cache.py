d6ca41de667f2b22d2a896cbad10aaa812cbfdb3
“victornnamdii” <victorilodiuba@gmail.com>
2023-02-21 22:30:44 +0100

    task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Learning Caching
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRU Caching System
+    """
+    def put(self, key, item):
+        """
+        Add an item to the cache
+        """
+        if key is not None and item is not None:
+            if key not in self.cache_data:
+                if len(self.cache_data) == BaseCaching.MAX_ITEMS:
+                    discarded = self.cache_data.popitem()[0]
+                    print("DISCARD: {}".format(discarded))
+            else:
+                del self.cache_data[key]
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        item = self.cache_data[key]
+        del self.cache_data[key]
+        self.cache_data[key] = item
+        return self.cache_data[key]
